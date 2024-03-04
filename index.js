e5879575ea570472d63591715782a1e84214d271
jajones49 <143618797+jajones49@users.noreply.github.com>
2024-03-04 15:39:28 +0000

    monday

---
+++ index.js
@@ -0,0 +1,19 @@
+let results=document.querySelector(".results")
+
+results.addEventListener("click", function() {
+
+let one=document.querySelector(".one").value
+let two=document.querySelector(".two").value
+
+
+if (one<30 && two==="Short")  
+{message.innerHTML="You are Squidward";}
+
+
+
+
+
+if (one>30 && two==="Short Tempered")  
+{message.innerHTML="You are Squidward";}
+
+});
